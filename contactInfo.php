aaf75346e2bf7319132762e6b673bd7ecfa04deb
Nazarii Konechnyi <nazar.konechniy2@gmail.com>
2024-04-20 23:30:30 +0100

    lab3

--- contactInfo.php
+++ contactInfo.php
@@ -26,10 +26,54 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (!empty($name) && !empty($email) && !empty($phoneNumber) && !empty($message)) {
         // Створення об'єкту ContactInformation з отриманими даними
         $contactInfo = new ContactInformation($name, $email, $phoneNumber, $message);
+        saveContactToDatabase($contactInfo);
 
     } else {
         echo "Please fill in all the fields.";
     }
 }
 
+function saveContactToDatabase(ContactInformation $contactInformation) {
+    // Підключення до бази даних SQLite
+    $db = new SQLite3('my_database.db');
+
+    // Перевірка наявності підключення до бази даних
+    if (!$db) {
+        echo "Помилка підключення до бази даних!";
+        return;
+    }
+
+    // Підготовка SQL-запиту для вставки даних
+    $sql = "INSERT INTO contacts (name, email, phoneNumber, message) VALUES (:name, :email, :phoneNumber, :message)";
+
+    // Підготовка SQL-запиту
+    $stmt = $db->prepare($sql);
+
+
+    // Прив'язка параметрів до значень
+    $stmt->bindParam(':name', $contactInformation->name);
+    $stmt->bindParam(':email', $contactInformation->email);
+    $stmt->bindParam(':phoneNumber', $contactInformation->phoneNumber);
+    $stmt->bindParam(':message', $contactInformation->message);
+
+    // Перевірка наявності підготовленого запиту
+    if (!$stmt) {
+        echo "Помилка підготовки SQL-запиту!";
+        return;
+    }
+
+    // Виконання SQL-запиту
+    $result = $stmt->execute();
+
+    // Перевірка результату виконання SQL-запиту
+    if ($result) {
+        echo "Дані успішно збережені в базі даних!";
+    } else {
+        echo "Помилка при збереженні даних в базу даних!";
+    }
+
+    // Закриття підготовленого запиту і з'єднання з базою даних
+    $stmt->close();
+    $db->close();
+}
 ?>
